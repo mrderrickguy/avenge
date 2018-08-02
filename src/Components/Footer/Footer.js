978b054d20a8e3283f8e9620b560a4528fd6ae2d
mrderrickguy <zakiahdg@gmail.com>
2018-08-02 10:08:49 -0500

    updating

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = props => (
     <div className="footers">
         <p>
         <h3 class="foot">Copyright &copy; 2018 Derrick Guy.</h3>
-            <a href="https://github.com/mrderrickguy/clickygame" target="_blank">GitHub Link</a>  
+            <a href="https://github.com/mrderrickguy/clickygame">GitHub Link</a>  
             
         </p>
     </div>
